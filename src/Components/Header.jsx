e2b311f1199aaad7f788d5a7801441f89740263d
Joynobjhorna <joynobjhorna8@gmail.com>
2026-07-27 23:11:20 +0600

    2nd edit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Container from './Container'
+
+const Header = () => {
+  return (
+    <div className="bg-black text-white">
+      <Container>
+        <div className="flex justify-between">
+          <div>
+
+          </div>
+          <p className="text-sm">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!<span>ShopNow</span></p>
+          <select name="" id="" className='bg-black text-white'>
+            <option value="">English</option>
+            <option value="">Bangla</option>
+            <option value="">spanish</option>
+
+          </select>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default Header
