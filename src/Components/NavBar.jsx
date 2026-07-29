1be141694cd7b85f79397cc8f71238af7afbf0f0
Joynobjhorna <joynobjhorna8@gmail.com>
2026-07-29 23:10:36 +0600

    navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -13,28 +13,28 @@ const NavBar = () => {
     <Container>
 
 
-      <div className=''>
+      <div className="flex justify-between py-7 items-center">
         <div>
           <img src={Logo} alt="Logo" />
         </div>
-        <ul>
+        <ul className="flex gap-12 ">
           <li>Home</li>
           <li>Contact</li>
           <li>About</li>
           <li>Sign Up</li>
 
         </ul>
-        <div>
-          <div className="relative">
+        <div className="flex items-center gap-6">
+          <div className="relative flex justify-between items-center pl-7 py-1.75 bg-[#F5F5F5] ">
 
 
-            <input type="text" placeholder='What are you looking for?' />
-            <HiMagnifyingGlass className="absulote" />
+            <input type="text" className="[34]"placeholder='What are you looking for?' />
+            <HiMagnifyingGlass className="absulote right-3 top-3 text-2xl" />
           </div>
 
-          <div className="flex justify-between">
-            <CiHeart />
-            <CiShoppingCart />
+          <div className="flex justify-between items-center gap-4">
+            <CiHeart className="text-3xl" />
+            <CiShoppingCart className="text-3xl"/>
           </div>
         </div>
       </div>
